2a87712503c6c8eb9f6989223fd397f779e64ece
Bill Agner <william_agner@hotmail.com>
2018-11-27 16:24:22 -0700

    Changed files based on Hashgraph interface changes.

--- hedera-sdk-dotnet-wrapper/DnHederaClient.h
+++ hedera-sdk-dotnet-wrapper/DnHederaClient.h
@@ -14,10 +14,9 @@ namespace hederasdkdotnetwrapper {
 
 		/// Establish a connection to a Hedera node.
 		/// Must be closed with [hedera_client_close].
-		static HederaClient* HederaClientDial(const char* address);
+		static HederaError HederaClientDial(const char* address, HederaClient**);
 
 		/// Close and releases resources for a [HederaClient].
 		static void HederaClientClose(HederaClient* client);
-
 	};
 }
